04566cbe99fa9a8ad97fea2f46253da8c7f1fb67
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-02 10:29:52 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -11,7 +11,7 @@ class PigLatinizer
     if first_syll_cons == nil
       pig_latin = word + 'way'
     else
-      pig_latin = word[..-1] + first_syll_cons + 'ay'
+      pig_latin = word[first_syll_cons_end_location..-1] + first_syll_cons + 'ay'
     end
     pig_latin
   end
