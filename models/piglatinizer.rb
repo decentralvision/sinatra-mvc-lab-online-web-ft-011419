d6495f250ae18cb59b79171566b423d2d43797e7
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-02 10:34:25 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,7 +6,9 @@ class PigLatinizer
   end
 
   def piglatinize(words)
-    words.collect! do |word|
+
+    words.split(' ').collect! do |word|
+
       first_syll_cons = word.downcase.scan(/\A[^aeiou]+/).first
       if first_syll_cons == nil
         pig_latin = word + 'way'
@@ -15,6 +17,9 @@ class PigLatinizer
         pig_latin = word[first_syll_cons_end_location..-1] + first_syll_cons + 'ay'
       end
       pig_latin
-    end
+
+    end.join(' ')
+
   end
+
 end
