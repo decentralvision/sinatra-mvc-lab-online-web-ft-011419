3f1e03d5e9ca771bad68ffaa36b701f7c25da2e9
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-02 08:26:34 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,7 +6,8 @@ class PigLatinizer
   end
 
   def piglatinize(word)
-    word[0].scan(/[aeiou]/).empty? ?  suffix = 'lay' : suffix = 'ay'
+    word[0].scan(/[aeiou]/).empty? ?  suffix = 'ay' : suffix = 'lay'
     pig_latin = word[1..-1] + word[0] + suffix
   end
 end
+ 
