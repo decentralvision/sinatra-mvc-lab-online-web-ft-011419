51b4b1fb9b41e2460e338cf6fb37bbd0bf41910d
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-02 08:17:45 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,7 +6,6 @@ class PigLatinizer
   end
 
   def piglatinize(word)
-    binding.pry
     word[0].scan(/[aeiou]/).empty? ?  suffix = 'lay' : suffix = 'ay'
     pig_latin = word[1..-1] + word[0] + suffix
   end
