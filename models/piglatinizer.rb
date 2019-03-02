3efc0948f2c326198e5deacaa537871e288d6a2d
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-02 10:18:33 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -9,7 +9,7 @@ class PigLatinizer
     first_syllable_consonant? = word.downcase.scan(/\A[^aeiou]+/).first
     if first_syllable_consonant? == nil
     else
-      pig_latin = word +first_consonant_syllable.first
+      pig_latin = word +first_consonant_syllable
     word[0].downcase ?  suffix = 'ay' : suffix = 'way'
 
   end
