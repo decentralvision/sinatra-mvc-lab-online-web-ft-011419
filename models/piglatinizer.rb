cb1ccc3192d599e31611090b44fcd8b89dd4cfd7
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-02 10:32:02 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,10 +8,11 @@ class PigLatinizer
   def piglatinize(word)
     first_syll_cons = word.downcase.scan(/\A[^aeiou]+/).first
     binding.pry
-    first_syll_cons_end_location = first_syll_cons.size
+
     if first_syll_cons == nil
       pig_latin = word + 'way'
     else
+      first_syll_cons_end_location = first_syll_cons.size
       pig_latin = word[first_syll_cons_end_location..-1] + first_syll_cons + 'ay'
     end
     pig_latin
