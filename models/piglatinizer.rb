3f974a9a340ee878c036ce4e23c8493b4a106f0a
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-02 10:22:19 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,7 +8,7 @@ class PigLatinizer
   def piglatinize(word)
     first_syllable_consonant? = word.downcase.scan(/\A[^aeiou]+/).first
     if first_syllable_consonant? == nil
-      pig_latin = word[0]
+      pig_latin = word + 'way'
     else
       pig_latin = word[(first_syllable_consonant?.size -1)..-1] + first_consonant_syllable + 'ay'
     end
