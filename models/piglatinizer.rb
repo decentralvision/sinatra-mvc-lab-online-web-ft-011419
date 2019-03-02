620ac7641eff89e5259774d7349485acb1527515
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-03-02 10:17:38 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,9 +7,9 @@ class PigLatinizer
 
   def piglatinize(word)
     first_consonant_syllable = word.downcase.scan(/\A[^aeiou]+/)
-    if first_consonant_syllable.empty?
+    if first_consonant_syllable == nil
     else
-      pig_latin = word first_consonant_syllable.first
+      pig_latin = word +first_consonant_syllable.first
     word[0].downcase ?  suffix = 'ay' : suffix = 'way'
 
   end
